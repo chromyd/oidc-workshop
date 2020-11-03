64d882053dd81acbe3760f9505d81699e4a48bc4
Dusan Chromy <dusan.chromy@yahoo.de>
2020-11-03 19:19:37 +0100

    Initial spring starter project

---
+++ src/main/java/de/dbsystel/oidcworkshop/OidcWorkshopApplication.java
@@ -0,0 +1,13 @@
+package de.dbsystel.oidcworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OidcWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OidcWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/dbsystel/oidcworkshop/OidcWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dbsystel.oidcworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OidcWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
