18a101616a7d24a23a0de6eb17c4b0124ccbe422
Dusan Chromy <dusan.chromy@yahoo.de>
2020-11-04 14:51:49 +0100

    Add exercise 2

---
+++ src/main/java/de/dbsystel/oidcworkshop/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package de.dbsystel.oidcworkshop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public WebSecurityConfigurerAdapter webSecurityConfigurer() {
+        return new WebSecurityConfigurerAdapter() {
+            @Override
+            protected void configure(HttpSecurity http) throws Exception {
+                http
+                        .authorizeRequests()
+                        .antMatchers("/").permitAll()
+                        .antMatchers("/private/info").authenticated()
+                        .anyRequest().denyAll()
+                        .and()
+                        .oauth2Login();
+            }
+        };
+    }
+}
